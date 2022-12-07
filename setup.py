1cd1601fbc8ed64040d2495970a95e19cde5e8aa
mas-veritas2 <93499422+mas-veritas2@users.noreply.github.com>
2022-12-07 12:45:27 +0800

    Updating version to 1.1.1 to create a new release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
 
 install_requires = [x.strip() for x in all_reqs]
 
-setup(name='veritastool',version='1.1.0',description ='Veritas Diagnosis tool for fairness assessment.',author='MAS Veritas',
+setup(name='veritastool',version='1.1.1',description ='Veritas Diagnosis tool for fairness assessment.',author='MAS Veritas',
 install_requires=install_requires,python_requires='>=3.7.5,<=3.9.5',
 packages=['veritastool.model','veritastool.fairness','veritastool.metrics','veritastool.config','veritastool.util','veritastool.resources'],
 include_package_data=True,
