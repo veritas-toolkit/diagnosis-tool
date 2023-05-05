dff532daeb1ee77c93a00262b25b9be2b4d992f3
masveritas2 <masveritas2@gmail.com>
2023-05-05 10:15:48 +0800

    update to Readme.md

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
 
 install_requires = [x.strip() for x in all_reqs]
 
-setup(name='veritastool',version='2.0.1',description ='Veritas Diagnosis tool for fairness & transparency assessment.',author='MAS Veritas',
+setup(name='veritastool',version='2.0.2',description ='Veritas Diagnosis tool for fairness & transparency assessment.',author='MAS Veritas',
 install_requires=install_requires,python_requires='>=3.8.0,<=3.10.10',
 packages=['veritastool.model','veritastool.principles','veritastool.metrics','veritastool.config','veritastool.util','veritastool.resources','veritastool.usecases'],
 include_package_data=True,
